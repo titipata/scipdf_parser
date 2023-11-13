9a49968757c152ed7b7b1dab02030500074fc16f
Luca Foppiano <lfoppiano@users.noreply.github.com>
2023-11-13 11:29:44 +0000

    Update serve_grobid.sh

--- serve_grobid.sh
+++ serve_grobid.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+# Recommended way to run Grobid is to have docker installed. See details here: https://grobid.readthedocs.io/en/latest/Grobid-docker/
 
-# Recommended way to run Grobid is Docker, https://grobid.readthedocs.io/en/latest/Grobid-docker/
+if ! command -v docker &> /dev/null; then
+    echo "Error: Docker is not installed. Please install Docker before running Grobid."
+    exit 1
+fi
 
-docker run --rm --gpus all --init --ulimit core=0 -p 8070:8070 grobid/grobid:0.7.3
+
+machine_arch=$(uname -m)
+
+if [ "$machine_arch" == "armv7l" ] || [ "$machine_arch" == "aarch64" ]; then
+    docker run --rm --gpus all --init --ulimit core=0 -p 8070:8070 grobid/grobid:0.7.3-arm
+else
+    docker run --rm --gpus all --init --ulimit core=0 -p 8070:8070 grobid/grobid:0.7.3
+fi
