823aaafe768804a0193d299b219e4ce981924d5b
Luca Foppiano <lfoppiano@users.noreply.github.com>
2023-11-13 11:23:59 +0000

    Update grobid runner
    using the docker version should solve some of the issues also with windows users

--- serve_grobid.sh
+++ serve_grobid.sh
@@ -2,4 +2,4 @@
 
 # Recommended way to run Grobid is Docker, https://grobid.readthedocs.io/en/latest/Grobid-docker/
 
-docker run --rm --gpus all --init --ulimit core=0 -p 8080:8070 grobid/grobid:0.7.3
+docker run --rm --gpus all --init --ulimit core=0 -p 8070:8070 grobid/grobid:0.7.3
